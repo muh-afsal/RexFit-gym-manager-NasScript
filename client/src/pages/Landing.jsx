644a7b8be6340ed48189d5568276fce5b5e69e67
Muhammed Afsal <afsalhdy@gmail.com>
2024-11-13 10:20:38 +0530

    updates:image alignment updated

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -4,7 +4,7 @@ import { ArrowRight } from "lucide-react";
 import { Link } from "react-router-dom";
 import singleDumbelImage from "../assets/images/singleDumbellImage.png";
 import weightLiftingImage from "../assets/images/Screenshot 2024-11-12 160936.png";
-import trainer1 from "../assets/images/Screenshot 2024-11-12 162557.png";
+import trainer1 from "../assets/images/Screenshot 2024-11-13 100512.png";
 import trainer2 from "../assets/images/Screenshot 2024-11-12 162637.png";
 import trainer3 from "../assets/images/Screenshot 2024-11-12 162732.png";
 import sendMessageBg from "../assets/images/Screenshot 2024-11-12 165858.png";
@@ -61,7 +61,7 @@ const Landing = () => {
             PROGRAM BY BEST PROFESSIONAL TRAINER AVAILABLE
           </h1>
         </div>
-        <div className="lg:h-[450px] h-[550px] w-full dark:bg-dark-bg flex justify-center items-center">
+        <div className="lg:h-[600px] h-[550px] w-full dark:bg-dark-bg flex justify-center items-center">
           <div className="lg:w-[70%] md:w-[90%] w-[95%] h-[90%] ">
             <div className="h-[30%] w-full  items-center flex px-5">
               <h1 className="font-michroma md:text-5xl text-2xl dark:text-white text-black font-extrabold">
@@ -103,8 +103,8 @@ const Landing = () => {
                     Professional Trainers
                   </h1>
                   <h1 className="font-michroma text-[10px]  ">
-                     Professional Trainers and their support,we give
-                    importance to out clients and their needs.
+                    Professional Trainers and their support,we give importance
+                    to out clients and their needs.
                   </h1>
                   <p className="font-michroma font-semibold text-[12px] flex   gap-1 cursor-pointer hover:text-black">
                     Learn More <ArrowRight size={18} className="mt-[2px]" />
@@ -145,7 +145,7 @@ const Landing = () => {
         >
           <div className="absolute inset-0 bg-black opacity-0 dark:opacity-50 z-[-10px]"></div>
         </div>
-        <div className="lg:h-[700px] h-max w-full dark:bg-dark-bg flex justify-center items-center">
+        <div className="lg:h-[700px] h-max pb-10 w-full dark:bg-dark-bg flex justify-center items-center">
           <div className="lg:w-[70%] md:w-[90%] w-[95%] h-[90%] ">
             <div className="h-[20%] w-full  items-center flex px-5">
               <h1 className="font-michroma md:text-5xl mt-6 text-2xl dark:text-white  w-full flex justify-center text-black font-extrabold">
@@ -154,34 +154,51 @@ const Landing = () => {
             </div>
             <div className=" items-center flex-col md:flex-row h-[1200px] md:h-[360px]  lg:h-[390px] w-full flex p-5 gap-7 md:gap-24  overflow-auto scrollbar-custom">
               <div
-                className="h-full lg:w-1/3 w-[300px]  px-8 rounded-xl "
+                className="h-full relative lg:w-1/3 w-[300px] px-8 rounded-xl transform transition-transform duration-300 hover:scale-105"
                 style={{
                   backgroundImage: `url(${trainer1})`,
                   backgroundSize: "cover",
                   backgroundPosition: "center",
                 }}
               >
-                <h1 className="font-michroma font-bold ml-1 text-rex-green">Mar Spector</h1>
+                <div className="w-full h-full flex items-end pb-3">
+                  <h1 className="font-michroma font-bold text-white">
+                    Mark Spector
+                  </h1>
+                </div>
+                <div className="absolute inset-0 bg-black opacity-40 dark:opacity-40 z-[-10px] rounded-lg"></div>
               </div>
+
               <div
-                className="h-full  lg:w-1/3 w-[300px]  px-8 rounded-xl"
+                className="h-full relative lg:w-1/3 w-[300px] px-8 rounded-xl transform transition-transform duration-300 hover:scale-105"
                 style={{
                   backgroundImage: `url(${trainer2})`,
                   backgroundSize: "cover",
                   backgroundPosition: "center",
                 }}
               >
-                <h1 className="font-michroma font-bold ml-1 text-rex-green">Jake Lockley</h1>
+                <div className="absolute inset-0 bg-black opacity-40 dark:opacity-40 z-[-10px] rounded-lg"></div>
+                <div className="w-full h-full  flex items-end pb-3">
+                  <h1 className="font-michroma font-bold  text-white">
+                    Jake Lockley
+                  </h1>
+                </div>
               </div>
               <div
-                className="h-full lg:w-1/3 w-[300px]  px-8 rounded-xl"
+                className="h-full relative lg:w-1/3 w-[300px] px-8 rounded-xl transform transition-transform duration-300 hover:scale-105"
                 style={{
                   backgroundImage: `url(${trainer3})`,
                   backgroundSize: "cover",
                   backgroundPosition: "center",
                 }}
               >
-                <h1 className="font-michroma font-bold ml-1 text-rex-green">Steven Grand</h1>
+                <div className="absolute inset-0 bg-black opacity-40 dark:opacity-40 z-[-10px] rounded-lg"></div>
+
+                <div className="w-full h-full  flex items-end pb-3">
+                  <h1 className="font-michroma font-bold  text-white">
+                    Steven Grand
+                  </h1>
+                </div>
               </div>
             </div>
           </div>
@@ -194,15 +211,16 @@ const Landing = () => {
             backgroundPosition: "center",
           }}
         >
-          <div className="w-full h-full  flex flex-col items-center justify-center gap-4">
-            <h1 className="font-michroma md:text-5xl mb-14 mt-6 text-center text-2xl text-white w-full md:w-[70%] flex justify-center  font-extrabold">
-              Get Your First Trial Visit
-            </h1>
+         <div className="absolute  inset-0 bg-black opacity-40 dark:opacity-50 z-[-50px]"></div>
+          <div className="w-full  h-full  flex flex-col items-center justify-center gap-4">
+          <h1 className="font-michroma  md:text-5xl mb-14 mt-6 text-center text-2xl text-white w-full md:w-[70%] flex justify-center  font-extrabold">
+            Get Your First Trial Visit
+          </h1>
             <div className="relative w-full md:w-[50%]">
               <input
                 type="text"
                 placeholder="Send message..."
-                className="w-full px-4 text-black  dark:bg-neutral-500/10 bg-neutral-100/10 backdrop-blur-lg py-2 pr-10 rounded-full border border-gray-300 focus:outline-none focus:border-rex-green"
+                className="w-full px-4 text-black  dark:bg-neutral-900/60 bg-neutral-700/80 backdrop-blur-lg py-2 pr-10 rounded-full border border-gray-300 focus:outline-none focus:border-rex-green"
               />
               <button
                 type="submit"
@@ -212,57 +230,58 @@ const Landing = () => {
               </button>
             </div>
           </div>
-          <div className="absolute inset-0 bg-black opacity-40 dark:opacity-60 z-[-10px]"></div>
         </div>
         <div className="h-[350px] w-full dark:bg-dark-bg  p-5">
           <div className="w-full h-full flex-col  flex items-end">
             <div className="w-full h-full   p-9">
               <div className="h-full w-full  flex">
-                 <div className="w-full md:w-[40%] h-full ">
-                   <RexFitLogo/>
-                   <h1 className="font-michroma dark:text-neutral-400 text-neutral-800 text-base mt-5  ">
-                     Professional Trainers and their support,we give
-                    importance to out clients and their needs.
+                <div className="w-full md:w-[40%] h-full ">
+                  <RexFitLogo />
+                  <h1 className="font-michroma dark:text-neutral-400 text-neutral-800 text-base mt-5  ">
+                    Professional Trainers and their support,we give importance
+                    to out clients and their needs.
                   </h1>
-                 </div>
-                 <div  className="w-[60%] h-full  md:flex hidden dark:text-neutral-400 text-neutral-800" >
-                     <div className="w-1/3 h-full flex items-center gap-6 p-3 flex-col">
-                     <h1 className="font-michroma text-sm font-bold dark:text-rex-green text-neutral-800">About us</h1>
-                     <div className="flex flex-col gap-1">
-
-                     <h1 className=" text-base font-light ">About</h1>
-                     <h1 className=" text-base font-light ">Blog</h1>
-                     <h1 className=" text-base font-light ">Contact</h1>
-                     <h1 className=" text-base font-light ">Vacancy</h1>
-                     </div>
-                     </div>
-                     <div className="w-1/3 h-full flex items-center gap-6 p-3 flex-col">
-                     <h1 className="font-michroma text-sm font-bold  dark:text-rex-green text-neutral-800">Services</h1>
-                     <div className="flex flex-col gap-1">
-
-                     <h1 className=" text-base font-light ">Attendance</h1>
-                     <h1 className=" text-base font-light ">PT programs</h1>
-                     <h1 className=" text-base font-light ">Explore</h1>
-                     <h1 className=" text-base font-light ">Security</h1>
-                     </div>
-                     </div>
-                     <div className="w-1/3 h-full flex items-center gap-6 p-3 flex-col">
-                     <h1 className="font-michroma text-sm font-bold  dark:text-rex-green text-neutral-800">Support</h1>
-                     <div className="flex flex-col gap-1">
-
-                     <h1 className=" text-base font-light ">Contact us</h1>
-                     <h1 className=" text-base font-light ">Instagram</h1>
-                     <h1 className=" text-base font-light ">Telegram</h1>
-                     <h1 className=" text-base font-light ">Whatsapp</h1>
-                     </div>
-                     </div>
-                    
-                 </div>
+                </div>
+                <div className="w-[60%] h-full  md:flex hidden dark:text-neutral-400 text-neutral-800">
+                  <div className="w-1/3 h-full flex items-center gap-6 p-3 flex-col">
+                    <h1 className="font-michroma text-sm font-bold dark:text-rex-green text-neutral-800">
+                      About us
+                    </h1>
+                    <div className="flex flex-col gap-1">
+                      <h1 className=" text-base font-light ">About</h1>
+                      <h1 className=" text-base font-light ">Blog</h1>
+                      <h1 className=" text-base font-light ">Contact</h1>
+                      <h1 className=" text-base font-light ">Vacancy</h1>
+                    </div>
+                  </div>
+                  <div className="w-1/3 h-full flex items-center gap-6 p-3 flex-col">
+                    <h1 className="font-michroma text-sm font-bold  dark:text-rex-green text-neutral-800">
+                      Services
+                    </h1>
+                    <div className="flex flex-col gap-1">
+                      <h1 className=" text-base font-light ">Attendance</h1>
+                      <h1 className=" text-base font-light ">PT programs</h1>
+                      <h1 className=" text-base font-light ">Explore</h1>
+                      <h1 className=" text-base font-light ">Security</h1>
+                    </div>
+                  </div>
+                  <div className="w-1/3 h-full flex items-center gap-6 p-3 flex-col">
+                    <h1 className="font-michroma text-sm font-bold  dark:text-rex-green text-neutral-800">
+                      Support
+                    </h1>
+                    <div className="flex flex-col gap-1">
+                      <h1 className=" text-base font-light ">Contact us</h1>
+                      <h1 className=" text-base font-light ">Instagram</h1>
+                      <h1 className=" text-base font-light ">Telegram</h1>
+                      <h1 className=" text-base font-light ">Whatsapp</h1>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-              <div className="h-[60px] bottom-0  w-full flex justify-center md:justify-start items-center md:pl-16  border-t dark:border-neutral-800 border-neutral-300">
-           <h1>Copyright@2024 All Right reserved</h1>
-              </div>
+            <div className="h-[60px] dark:text-neutral-600 text-neutral-300 bottom-0  w-full flex justify-center md:justify-start items-center md:pl-16  border-t dark:border-neutral-800 border-neutral-300">
+              <h1>Copyright@2024 All Right reserved</h1>
+            </div>
           </div>
         </div>
       </div>
